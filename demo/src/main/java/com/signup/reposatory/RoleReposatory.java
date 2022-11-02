ba30185eaaf4a66d385fc494a7284b97290c6aaf
savitsharma <64885029+savitsharma@users.noreply.github.com>
2022-11-02 21:26:10 +0530

    Delete demo directory

--- demo/src/main/java/com/signup/reposatory/RoleReposatory.java
+++
@@ -1,8 +0,0 @@
-package com.signup.reposatory;
-
-import com.signup.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleReposatory extends JpaRepository<Role,Long> {
-    Role findByName(String name);
-}
